354bda3ee1143755c7614b44079e961f6f5cb12d
Karthik <karthikwebdev@outlook.com>
2023-04-20 15:33:41 +0530

    Update Route and add react-toasitfy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 import Explore from "./pages/Explore";
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
@@ -15,14 +17,14 @@ function App() {
     <Routes>
       <Route path='/' element={<Explore />} />
       <Route path='/offers' element={<Offers />} />
-      <Route path='/profile' element={<SignIn />} />
+      <Route path='/profile' element={<Profile />} />
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/forgot-password' element={<ForgotPassword />} />
     </Routes>
     <Navbar />
      </Router>
-   
+    <ToastContainer hideProgressBar={true}/>
    </>
   );
 }
