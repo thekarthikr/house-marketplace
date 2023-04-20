91771693844735f54600747884eb9900d964620f
Karthik <karthikwebdev@outlook.com>
2023-04-20 17:37:17 +0530

    Add custom hook

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react"
+
+import { onAuthStateChanged , getAuth } from "firebase/auth"
+
+export const useAuthStatus = () => {
+
+    const [loggedIn , setLoggedIn]  = useState(false)
+    const [checkingStatus , setCheckingStatus] = useState(true)
+
+ useEffect(()=>{
+    const auth = getAuth()
+    onAuthStateChanged(auth,(user)=>{
+        if(user){
+            setLoggedIn(true)
+
+        }
+        setCheckingStatus(false)
+    })
+ })
+
+  return {loggedIn , checkingStatus}
+}
