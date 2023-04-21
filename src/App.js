d7915243dc3f00d36e169c38a6b83bcecf0afb0f
Karthik <karthikwebdev@outlook.com>
2023-04-21 19:48:51 +0530

    add route for contact page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Navbar from "./components/Navbar"
 import PrivateRouter from "./components/PrivateRouter";
 import CreateListing from "./pages/CreateListing"
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact"
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
       <Route path='/forgot-password' element={<ForgotPassword />} />
       <Route path='/create-listing' element={<CreateListing /> } />
       <Route  path="/category/:categoryName/:listingId" element={<Listing />} />
+      <Route path="/contact/:landlordId" element={<Contact />} />
     </Routes>
     <Navbar />
      </Router>
