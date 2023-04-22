175dfc930e192f33e12e939f91dd0003397200df
Karthik <karthikwebdev@outlook.com>
2023-04-22 12:36:55 +0530

    Add route for edit listing

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import PrivateRouter from "./components/PrivateRouter";
 import CreateListing from "./pages/CreateListing"
 import Listing from "./pages/Listing";
 import Contact from "./pages/Contact"
+import EditListing from "./pages/EditListing";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
       <Route path='/forgot-password' element={<ForgotPassword />} />
       <Route path='/create-listing' element={<CreateListing /> } />
       <Route  path="/category/:categoryName/:listingId" element={<Listing />} />
+      <Route path="/edit-listing/:listingId" element={<EditListing />} />
       <Route path="/contact/:landlordId" element={<Contact />} />
     </Routes>
     <Navbar />
